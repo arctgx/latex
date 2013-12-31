f8c459dfb3e2523717453f4abb0153301dc8bcb7
arctgx <arctgx@126.com>
2013-12-31 17:28:27 +0800

    add tmp files

---
+++ zaizhitianxia/make_all.bat
@@ -0,0 +1,7 @@
+del *.aux
+del *.log
+del *.out
+del *.toc
+pdflatex book_all.tex
+pdflatex book_all.tex
+book_all.pdf
---
+++ zaizhitianxia/make_simple.bat
@@ -0,0 +1,7 @@
+del *.aux
+del *.log
+del *.out
+del *.toc
+pdflatex book_simple.tex
+pdflatex book_simple.tex
+book_simple.pdf
